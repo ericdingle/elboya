568d08b9a228f457b71c98479d557ffac19e415c
Eric Dingle <eric.dingle@gmail.com>
2012-12-27 18:07:08 -0700

    Integrate jQuery UI and DataTable.

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,61 @@
 (function() {
 
 $(document).ready(function() {
-  $('#add_torrent').click(addTorrent);
-  updateTorrents();
-});
+  // UI.
+  $('#tabs').tabs();
 
-function addTorrent() {
-  var magnet_url = $('#magnet_url').val();
-  var save_path = $('#save_path').val();
-  $.post('ajax/add_torrent', {magnet_url: magnet_url, save_path: save_path});
-}
+  $('#torrents').dataTable({
+    bFilter: false,
+    bInfo: false,
+    bPaginate: false,
+    bSort: false,
+  });
+
+  updateTorrents();
+
+  // Event handlers.
+  $('#add_torrent').button().click(addTorrent);
+});
 
 function updateTorrents() {
   $.get('ajax/get_torrents', function(data) {
-    while ($('#torrents tr').length != 1) {
-      $('#torrents tr:last').remove();
-    }
+    var table = $('#torrents').dataTable();
+    table.fnClearTable();
 
     for (var i = 0; i < data.length; ++i) {
       var torrent = data[i];
-      var row = $(
-          '<tr>' +
-            '<td>' + torrent.name + '</td>' +
-            '<td>' + util.toPercentage(torrent.status.progress) + '</td>' +
-            '<td>' + util.toByteRate(torrent.status.download_rate) + '</td>' +
-            '<td>' + util.toByteRate(torrent.status.upload_rate) + '</td>' +
-            '<td>' + torrent.status.num_peers + '</td>' +
-            '<td>' + torrent.status.state + '</td>' +
-          '</tr>');
-      $('#torrents').append(row);
+      var status = torrent.status;
+      table.fnAddData([
+        torrent.name,
+        util.toPercentage(status.progress),
+        util.toByteRate(status.download_rate),
+        util.toByteRate(status.upload_rate),
+        status.num_peers,
+        status.state
+      ])
     }
 
     setTimeout(updateTorrents, 1000);    
   }, "json");
+}
+
+function addTorrent(event) {
+  $('#add_torrent').button({disabled: true});
+
+  var magnet_url = $('#magnet_url').val();
+  var save_path = $('#save_path').val();
+
+  $.post('ajax/add_torrent', {magnet_url: magnet_url, save_path: save_path},
+         addTorrentSuccess);
+}
+
+function addTorrentSuccess() {
+  $('#add_torrent').button({disabled: false});
+
+  $('#magnet_url').val('');
+  $('#save_path').val('');
+
+  $('#tabs').tabs({active: 0});
 }
 
 })();
