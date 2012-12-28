186e8ec10afe3cc7f751f39cc5ab1a51a15132d4
Eric Dingle <eric.dingle@gmail.com>
2012-12-28 16:53:24 -0700

    Make percentage columns use mRender.

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
       {mData: 'name',
        sWidth: '30%'},
       {mData: 'status.progress',
+       mRender: formatPercentage,
        sWidth: '20%'},
       {mData: 'status.download_rate',
        mRender: formatByteRate,
@@ -30,7 +31,6 @@ $(document).ready(function() {
     bInfo: false,
     bPaginate: false,
     bSort: false,
-    fnRowCallback: formatPercentage,
     sAjaxDataProp: 'torrents',
     sAjaxSource: 'ajax/get_torrents',
   });
@@ -42,22 +42,25 @@ $(document).ready(function() {
   $('#add_torrent').button().click(addTorrent);
 });
 
-function formatPercentage(row, data) {
-  var div = $(
-      '<div>' +
-        '<span class="ui-progressbar-label">' +
-          util.toPercentage(data.status.progress) +
-        '</span>' +
-      '</div>');
+function formatPercentage(data, type, full) {
+  if (type == 'display') {
+    var div = $(
+        '<div>' +
+          '<span class="ui-progressbar-label">' +
+            util.toPercentage(data) +
+          '</span>' +
+        '</div>');
 
-  var value = Math.round(data.status.progress * 100);
-  div.progressbar({value: value});
+    var value = Math.round(data * 100);
+    div.progressbar({value: value});
 
-  $('td:eq(1)', row).html(div);
+    return div.wrap('<div>').parent().html();
+  }
+
+  return data;
 }
 
 function formatByteRate(data, type, full) {
-  // Return the formatted data for display.
   if (type == 'display')
     return util.toByteRate(data);
 
